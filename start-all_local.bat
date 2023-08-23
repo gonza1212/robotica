6005185c77b0bee344ee63a6247d4623af168eb5
gonza1212 <enano.g16@gmail.com>
2023-08-23 10:20:02 -0300

    Inicio de proyecto

---
+++ start-all_local.bat
@@ -0,0 +1,8 @@
+start cmd /k npm run dev
+start cmd /k php artisan serve
+cd "C:\xampp"
+xampp_start
+start http://localhost/phpmyadmin/
+start www.google.com.ar
+cd "C:\Users\Gonzalo\AppData\Local\Programs\Microsoft VS Code\"
+Code.exe
